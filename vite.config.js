14c0f0b79104a220b06096326c2f958fde3b592b
Adrian.V <adrian.radu.vasiu@students.beaminstitute.org>
2024-08-19 15:10:38 +0200

    added local and digital ocean deployment ip to vite config api

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://walrus-app-lqhsg.ondigitalocean.app:8080',
-        // target: 'http://192.168.178.42:4500', // Adjust this to your backend's actual address and port
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       // target: 'https://walrus-app-lqhsg.ondigitalocean.app:8080',
+  //       target: 'http://192.168.178.42:4500', // Adjust this to your backend's actual address and port
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // },
   resolve: {
     alias: {
       '@': '/src',
